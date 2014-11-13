4dd6d0c24ccd600a9996f3d93bd80afdcfa6d6c9
Evan Coleman <evan@evancoleman.net>
2014-11-12 21:17:45 -0500

    Fix layout and add fonts

--- Lights/RoomHeaderView.m
+++ Lights/RoomHeaderView.m
@@ -29,35 +29,39 @@
         [self.contentView addSubview:_titleLabel];
         
         _onButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        _onButton.translatesAutoresizingMaskIntoConstraints = NO;
         [_onButton setTitle:NSLocalizedString(@"On", nil) forState:UIControlStateNormal];
         [_onButton sizeToFit];
         [self.contentView addSubview:_onButton];
         
         _offButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        _offButton.translatesAutoresizingMaskIntoConstraints = NO;
         [_offButton setTitle:NSLocalizedString(@"Off", nil) forState:UIControlStateNormal];
         [_offButton sizeToFit];
         [self.contentView addSubview:_offButton];
         
         _editButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        _editButton.translatesAutoresizingMaskIntoConstraints = NO;
         [_editButton setTitle:NSLocalizedString(@"Edit", nil) forState:UIControlStateNormal];
         [_editButton sizeToFit];
         [self.contentView addSubview:_editButton];
         
-        self.contentView.layoutMargins = UIEdgeInsetsMake(0, 4, 0, 4);
-        
         RAC(self.titleLabel, text) = RACObserve(self, viewModel.name);
         
         [_editButton autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:28];
+        [_editButton autoSetDimension:ALDimensionWidth toSize:_editButton.bounds.size.width];
         [_editButton autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
         
         [_offButton autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:_editButton withOffset:-28];
+        [_offButton autoSetDimension:ALDimensionWidth toSize:_offButton.bounds.size.width];
         [_offButton autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
-        
+
         [_onButton autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:_offButton withOffset:-28];
+        [_onButton autoSetDimension:ALDimensionWidth toSize:_onButton.bounds.size.width];
         [_onButton autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
-        
+
         [_titleLabel autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsMake(0, 14, 0, 0) excludingEdge:ALEdgeRight];
-        [_titleLabel autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:_onButton withOffset:-10];
+//        [_titleLabel autoPinEdge:ALEdgeRight toEdge:ALEdgeLeft ofView:_onButton withOffset:-10];
     }
     return self;
 }
--- Lights/UIFont+Lights.m
+++ Lights/UIFont+Lights.m
@@ -10,4 +10,12 @@
 
 @implementation UIFont (Lights)
 
++ (UIFont *)lights_regularFontWithSize:(CGFloat)size {
+    return [UIFont fontWithName:@"AvenirNext-Regular" size:size];
+}
+
++ (UIFont *)lights_boldFontWithSize:(CGFloat)size {
+    return [UIFont fontWithName:@"AvenirNext-DemiBold" size:size];
+}
+
 @end
